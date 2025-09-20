fce7e11d74df1d0252e6b8d697f7fc8a8782e4e8
Aaraan Mahmood <151668629+aairhann17@users.noreply.github.com>
2025-09-20 15:52:36 -0400

    Update bulb.java

--- bulb.java
+++ bulb.java
@@ -1,17 +1,18 @@
 public class bulb implements factory{
     private final String id;
-    private double batry;
+    private double battery;
 
     public bulb(String id) {
         this.id = id;
     }
 
-    public void poweruse(double batry) {
-        this.batry = batry;
+    public void poweruse(double battery) {
+        this.battery = battery;
     }
 
     @Override
     public void make() {
-        System.out.println("A "+id+" brand bulb was made with a battery power of "+batry);
+        System.out.println("A "+id+" brand bulb was made with a battery power of "+battery);
     }
 }
+
